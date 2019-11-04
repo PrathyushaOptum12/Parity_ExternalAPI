01344f67d1049814bfc76e631c8391eba77d897f
Korrapati <prathyusha_korrapati@optum.com>
2019-11-04 15:00:42 -0500

    minor change

--- api/controllers/helloBlockchainController.js
+++ api/controllers/helloBlockchainController.js
@@ -9,6 +9,7 @@ var web3 = new Web3(); // instantiate Web3 to get a new object
 
 web3 = new Web3(new Web3.providers.WebsocketProvider("wss://accumdev.blockchain.azure.com:3300/ajkJzpNwg0SihtLDBc1dFTQC"));
 //web3Local = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
 var helloBlockchainContract = new web3.eth.Contract(abi,"0xc3DF3b7740040Af900e85E9F9a268AD2C7aFB277");
 //var helloBlockchainLocalContract = new web3Local.eth.Contract(abi,"0xEA04A65E212903A8411AFF832E0126Fd502EFbc1");
 
